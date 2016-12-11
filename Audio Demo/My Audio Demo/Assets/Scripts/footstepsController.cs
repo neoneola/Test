b303fc19f984fe3f9d02d19a5597792ac94f249b
Nicholas Zhang <nicholas.zhang1993@gmail.com>
2016-12-11 00:52:27 -0800

    added the audio demo scene

---
+++ Audio Demo/My Audio Demo/Assets/Scripts/footstepsController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class footstepsController : MonoBehaviour {
+
+	[SerializeField] private AudioClip[] clips;
+	private AudioSource source;
+	private AudioClip theClip;
+
+	// Use this for initialization
+	void Start () {
+	
+		source = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlayFootstepSound()
+	{
+		int x = Random.Range(1,5);
+		theClip = clips[x];
+		source.PlayOneShot(theClip);
+	}
+
+}
