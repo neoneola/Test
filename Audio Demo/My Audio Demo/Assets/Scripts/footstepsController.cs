3e3da0e01562033df30896f513644a1d6f22bf6c
Nicholas Zhang <nicholas.zhang1993@gmail.com>
2016-12-15 23:35:29 -0800

    [audio demo] footsteps clips can be played with a given physic material

--- Audio Demo/My Audio Demo/Assets/Scripts/footstepsController.cs
+++ Audio Demo/My Audio Demo/Assets/Scripts/footstepsController.cs
@@ -1,30 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable] public class footstepClipsOfOneMaterial
+{
+	//[SerializeField] private string materialName;
+	public PhysicMaterial Material;
+	public AudioClip[] Clips;
+}
 
 public class footstepsController : MonoBehaviour 
-	{
-
-	[SerializeField] private AudioClip[] clips;
+{
+	public PhysicMaterial theCollidedMaterial;
+	//[SerializeField] private footstepClipsOfOneMaterial[] MaterialList;
+	public List<footstepClipsOfOneMaterial> MaterialList;
 	private AudioSource source;
+	private AudioClip[] theClips;
 	private AudioClip theClip;
 
-	// Use this for initialization
-	void Start () {
-	
-		source = GetComponent<AudioSource>();
 
+	// Use this for initialization
+	void Start ()
+	{
+		source = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	/*
+	void Update()
+	{
+		RaycastHit hit;
+		Ray GroundDetectorRay = new Ray(transform.position, Vector3.down);
+		if(Physics.Raycast(GroundDetectorRay, out hit, 5f))
+		{
+			if(hit.collider.gameObject.tag == "Ground")
+			{
+				theCollidedMaterial = hit.collider.material;
+			}
+		}
 	}
+	*/
+
+	public AudioClip[] GetTheClips(PhysicMaterial p)
+	{
+		foreach (var footstepClipsOfOneMaterial in MaterialList)
+		{
+			if (footstepClipsOfOneMaterial.Material == p)
+			{
+				footstepClipsOfOneMaterial thelist = footstepClipsOfOneMaterial;
+				return thelist.Clips;
+			}
+		}
+		return null;
+	}
+
 
 	public void PlayFootstepSound()
 	{
-		int x = Random.Range(1,4);
-		theClip = clips[x];
+		theClips = GetTheClips(theCollidedMaterial);
+		int x = Random.Range(0, theClips.Length-1);
+		theClip = theClips[x];
 		source.PlayOneShot(theClip);
 	}
 
-    }
+}
