94946335de79bdc1fa61fdce5205754a1a81b58c
Ke Li <Ke_Li@thecdm.ca>
2017-02-27 17:23:30 -0800

    nick file added

---
+++ Audio Demo/My Audio Demo/Assets/Scripts/ChangeFootStepMaterial.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeFootStepMaterial : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	/*
+	void OnCollisionEnter(Collider coll)
+	{
+		if(coll.gameObject.tag =="Player")
+		{
+			footstepsController.AssignMaterial(GetComponent<Collider>().material);
+			Debug.Log("changed");
+		}
+	}
+*/
+}
